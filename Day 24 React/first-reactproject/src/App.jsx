421cfbeecbd91495e08ca6b16099186643e014db
Cipherr674 <71639314+Cipherr674@users.noreply.github.com>
2025-01-21 15:26:54 +0530

    first commit

---
+++ Day 24 React/first-reactproject/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Login from './components/Login'
+import Register from './components/Register'
+import './App.css';
+
+const App = () => {
+  return (
+  <>
+  <Login/>
+  <Register/>
+  
+  </>
+  )
+}
+
+export default App
